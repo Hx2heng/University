bf8f3f261d785ab63510baa3ed8a800c167ed822
hxz <397323988@qq.com>
2016-05-06 17:41:54 +0800

    first commit

---
+++ src/js/articleForm.js
@@ -0,0 +1,15 @@
+requirejs.config({
+	paths: {
+		jquery:'../lib/js/jquery.min',
+		bootstrap: '../lib/js/bootstrap.min',
+		ueditor:'../lib/js/ueditor.all.min',
+		ueditorconfig:'../lib/js/ueditor.config'
+	}
+});
+
+require(["jquery","bootstrap","ueditor","ueditorconfig"], function($,bootstrap,ueditor,ueditorconfig) {
+	console.log(UE);
+
+	//Editor
+	var ue = UE.getEditor('editor');
+});
